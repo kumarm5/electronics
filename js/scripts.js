2e9200e0b35fa3cddefc1a6d622a24ea9c287702
Bipul Kumar <Bipul.Kumar@fivecomms.com>
2017-11-19 16:14:08 +0530

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	
+	//backstretch the page
+	$.backstretch([
+	"img/services_top.jpg"
+	,"img/slider1_4.jpg"
+	, "img/slider2.jpg"
+	, "img/slider3.jpg"
+	, "img/slider4.jpg"
+  ], {duration: 3000, fade: 750});
+  
+});
